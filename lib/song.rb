c6ac62f4d17d8804eb48b2db33c13710ba8995fb
Yvonne Chen <yvonnechen1991@gmail.com>
2021-01-13 23:36:08 -0600

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -30,8 +30,11 @@ class Song
     end 
 
     def self.genre_count
+        # genre_count = Hash.new(0)
+        # @@genres.each {|genre| genre_count[genre] += 1}
+        # genre_count
         genre_count = {}
-          @@genres.each do |genre|
+        @@genres.each do |genre|
             if genre_count[genre] 
                 genre_count[genre] += 1
             else
@@ -39,12 +42,12 @@ class Song
             end         
         end
         genre_count
-    #      genre_count = Hash.new(0)
-    # @@genres.each {|genre| genre_count[genre] += 1}
-    # genre_count
     end
 
     def self.artist_count
+        # artist_count = Hash.new(0)
+        # @@artists.each {|artist| artist_count[artist] += 1}
+        # artist_count
         artist_count = {}
         @@artists.each do |artist|
             if artist_count[artist]
